bb18b2a4714f8cc5b70369c68929115796b60fc0
Roman Pronin <4prostore@gmail.com>
2020-12-03 22:02:49 +0300

    format

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int payment;
-        payment = 1500;
-        int limit;
-        limit = 1000;
+        int payment = 1500;
+        int limit = 1000;
         int bonus;
 
         if (payment > limit) {
-            bonus = balance + payment + payment/100;
-        }
-        else {bonus = balance + payment;
+            bonus = balance + payment + payment / 100;
+        } else {
+            bonus = balance + payment;
         }
         System.out.println(bonus);
     }
